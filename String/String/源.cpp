d4fb509bbe08655aa969af5705e8da548f915ab8
DMHACYJ <49850734+DMHACYJ@users.noreply.github.com>
2019-11-03 23:51:09 +0800

    Add files via upload

--- String/String/源.cpp
+++ String/String/源.cpp
@@ -9,7 +9,7 @@ void testString1() {
 	string s;
 	string s2("hello world");
 	string copy(s2);
-	string s2(s2, 0, 5);//hello
+	string s3(s2, 0, 5);//hello
 	string s4("hello world", 2);//he
 	string s5(10, 'a');//aaaaaaaaaa
 
@@ -43,11 +43,17 @@ void testIterator() {
 		++rit;
 	}
 	cout << endl;
-	for (int i = 0; i < s2.size(); i++)
+	for (unsigned int i = 0; i < s2.size(); i++)
 	{
 		cout << s2[i] << " ";
 	}
 	cout << endl;
+
+	//范围for
+	for (auto& ch : s2) {//引用类型的才可写，不加引用只可读
+		ch = '1';
+	}
+	cout << s2 << endl;
 
 	//const迭代器只读
 	const string s3("hello");
@@ -72,7 +78,167 @@ void testIterator() {
 	cout << endl;
 }
 
+void testCap() {
+	//reserve：只增加容量，不会减少容量，不会改变容量
+	//				  减少增容次数，提高效率
+	//string增容逻辑：初始大小15,第二次两倍，剩余1.5倍
+
+	string s("123");
+	cout << s.size() << endl;//3
+	cout << s.length() << endl;//3
+	//cout << s.max_size() << endl;
+	cout << s.capacity() << endl;//15
+
+	s.resize(10);//改变size，不改变容量
+	cout << s.size() << endl;//10
+	cout << s.capacity() << endl;//15
+	//输出运算符重载了，按size输出
+	cout << s << "end" << endl;//123       end
+	s.resize(10,'1');
+	cout << s << "end" << endl;//123111111
+
+	s.resize(1);
+	cout << s << "end" << endl;//1end
+
+	s.resize(20);
+	cout << s.size() << endl;//20
+	cout << s.capacity() << endl;//31
+	cout << s << "end" << endl;//1                   end
+
+	char str[] = "12345\0\0\0\0\0";
+	cout << str << "end" << endl;//12345end
+
+	//reserve：只增加容量，不会减少容量，不会改变容量
+	//跟resize相比，是直接给的容量，没有增加容量的过程，增强了效率
+	s.reserve(50);//不改变size改变容量
+	cout<< "s.reserve(50);" <<endl;
+	cout << s.size() << endl;
+	cout << s.capacity() << endl;
+
+	string s2;
+	int cap = s2.capacity();
+	//s2.reserve(100);
+	cout << cap << endl;
+	for (int i = 0; i < 100; i++) {
+		s2.push_back(i);//push_back只能差入单个字符
+		if (cap != s2.capacity()) {
+			cap = s2.capacity();
+			cout << cap << endl;
+		}
+	}
+
+	s2.shrink_to_fit();
+	cout << "s2.shrink_to_fit();" << endl;
+	cout << s2.capacity() << endl;
+
+	cout << s2.size() << endl;
+	cout << s2.capacity() << endl;
+	//clear只清空内容，size变成0，
+	//		   不释放内存，capacity不变
+	s2.clear();
+	cout << s2.size() << endl;
+	cout << s2.capacity() << endl;
+
+	//shrink_to_fit 内置判断：size和capacity量级一样的时候不减少容量
+	//														     量级不一样就减少到size最合适的量级
+	s2.shrink_to_fit();
+	cout << "s2.shrink_to_fit();" << endl;
+	cout << s2.capacity() << endl;
+}
+
+void testAcc() {
+	string s("123456");
+	cout << s[2] << endl;
+	cout << s.at(2) << endl;
+}
+
+void testModl() {
+//	string s;
+//	s.append("012");
+//	cout << s << endl;
+//	string s2("345");
+//	s.append(s2);
+//	cout << s << endl;
+//	string s3("1234567");
+//	s.append(s3, 5, 2);
+//	//数组天然的是一个迭代器
+//	char str[] = { "8910" };
+//	s.append(str, str + sizeof(str) / sizeof(str[0]));//迭代器 
+////   append(str第一个元素的位置, str最后一个元素的下一个位置)
+//	cout << s << endl;
+
+	////insert(pos, string)：在pos前面插入新的字符串
+	//string s("123");
+	//cout << s << endl;
+	//s.insert(0, "0");
+	//cout << s << endl;
+	//s.insert(0, "-1-2");
+	//cout << s << endl;
+	//string s2("456");
+	//s.insert(8, s2);
+	//cout << s << endl;
+	//s.insert(s.begin() + 4, '+');//迭代器只能插入字符
+	//cout << s << endl;
+	//char str[] = { "78910" };
+	////	s.append(str, str + sizeof(str) / sizeof(str[0]));
+	//s.insert(s.end(), str, str + sizeof(str) / sizeof(str[0]));
+	//cout << s << endl;
+
+	//string s("123");
+	//cout << s << endl;
+	//string s2("45");
+	////s.assign(s2);
+	//s = s2;
+	//cout << s << endl;
+
+	//string s;
+	//s += '0';
+	//cout << s << endl;
+	//s += "123";
+	//cout << s << endl;
+	//s += string("string");//string 生命周期很短，只存在于本行
+	//cout << s << endl;
+
+	//s.erase(0, 2);//删除了前两个元素
+	//cout << s << endl;
+	//s.erase(s.begin());//删除了第一个元素
+	//cout << s << endl;
+	//s.erase(s.begin() + 1, s.end() - 1);//除第一个和最后一个以外都删除了
+	//cout << s << endl;
+
+	string s("01234");
+	//cout << s << endl;
+	//string s2("56789");
+	//s.replace(3, 1, s2);//用s2替换s从第3个位置开始的一个字符："012567894"
+	//cout << s << endl;
+	//swap(s, s2);
+
+	//string s1("123");
+	//string s2("45");
+	//cout << (s1 < s2) << endl;
+
+	string s1;
+	string s2;
+	cin >> s1;//遇到空格读取结束
+	getline(cin, s2);//默认遇到换行读取结束
+	cout << s1 << endl;
+	cout << s2 << endl;
+}
+
+/*常用接口
+构造，拷贝，字符串初始化
+迭代器：
+begin end
+容量：size, resize, reserve
+元素访问：operator[]
+修改：operator+=, pop_back, swap(成员函数的)
+其它操作：c_str, find, substr, <, >, <=, ==, !=, >>, <<, getline
+*/
+
 int main() {
-	testString1();
+	//testString1();
+	testModl();
+	getchar();
 	return 0;
 }
+
